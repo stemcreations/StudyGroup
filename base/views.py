f30ad3ad8a9a0da5d471c0ac669c99ca7ab3e6b2
wesleycarothers <wesleycarothers@outlook.com>
2023-09-12 11:03:33 -0600

    added api

--- base/views.py
+++ base/views.py
@@ -24,6 +24,7 @@ def loginPage(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            
             return redirect('home')
         else:
             messages.error(request, 'Username OR password is incorrect')
